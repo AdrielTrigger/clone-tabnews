58f5a9bdfdcf4753404b02312ff1e7680ace1385
Adriel Bruno <adril_trigger@yahoo.com>
2025-10-26 00:38:39 -0300

    definidas variáveis de ambiente para conexão e requisições ao banco de dados local

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({
     chave:
       "se estiver lendo isso, é porque você conseguiu fazer a página de status da api funcionar",
