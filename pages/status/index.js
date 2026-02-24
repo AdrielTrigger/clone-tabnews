7af908f0e56d62fe89f35f1863e2c71795d66088
Adriel <adril_trigger@yahoo.com>
2026-02-23 23:25:50 -0300

    feat: show formated info on the status page

---
+++ pages/status/index.js
@@ -0,0 +1,54 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdatedAt />
+      <DatabaseVersion />
+    </>
+  );
+}
+
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let updatedAtText = "Loading...";
+
+  if (!isLoading && data) {
+    updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+  return <div>Last update: {updatedAtText}</div>;
+}
+
+function DatabaseVersion() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let databaseVersionText = "Loading...";
+  let maxConnText = "Loading...";
+  let openConnText = "Loading...";
+
+  if (!isLoading && data) {
+    databaseVersionText = data.dependencies.database.version;
+    maxConnText = data.dependencies.database.max_connections;
+    openConnText = data.dependencies.database.opened_connections;
+  }
+
+  return (
+    <div>
+      Database version: {databaseVersionText}, Maximum connections:{" "}
+      {maxConnText}, Opened connections: {openConnText}
+    </div>
+  );
+}
